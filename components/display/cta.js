50ebaac71b783646e986aefd99e1cf250895767e
mohansky <38560439+mohansky@users.noreply.github.com>
2023-03-13 11:09:10 +0530

    Add files via upload

--- components/display/cta.js
+++ components/display/cta.js
@@ -3,7 +3,7 @@ import Modal from "./modal";
 export default function CTA() {
   return (
     <>
-      <section className="relative bg-blue-teal-gradient px-4 sm:px-8 lg:px-16 xl:px-40 2xl:px-64 py-12 text-center md:text-left">
+      <section className="relative bg-gradient-to-r from-scblue via-scblue to-blue-500 px-4 sm:px-8 lg:px-16 xl:px-40 2xl:px-64 py-12 text-center md:text-left">
         <div className="md:flex md:items-center md:justify-center">
           <h2 className="text-xl font-bold text-white">
             Book an Appointment! <br className="block md:hidden" />
