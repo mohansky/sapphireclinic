78c64d82f524c83d4d9b02472b921ad7a872bf71
mohansky <38560439+mohansky@users.noreply.github.com>
2023-03-22 23:06:51 +0530

    Delete components/display directory

--- components/display/page-title.js
+++
@@ -1,7 +0,0 @@
-export default function PageTitle({ children }) {
-  return (
-    <h2 className="uppercase text-scblue text-5xl text-center font-extrabold mt-28 mb-12">
-      {children}
-    </h2>
-  );
-}
--- components/display/treatment-title.js
+++
@@ -1,9 +0,0 @@
-export default function TreatmentTitle({ post }) {
-  return (
-    <>
-      <li className="mb-2">
-         {post.frontmatter.title} 
-      </li>
-    </>
-  );
-}
