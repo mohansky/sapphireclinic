1b03e9940a61a123bf0e3527358be9a4e4375d62
mohansky <38560439+mohansky@users.noreply.github.com>
2023-04-03 01:22:45 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
 }
 
 module.exports = nextConfig
