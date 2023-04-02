a79e868e55fabc46277be2e790e27f9ddc9cb9f7
mohansky <38560439+mohansky@users.noreply.github.com>
2023-04-03 00:49:13 +0530

    Delete components directory

--- components/display/page-title.js
+++
@@ -1,8 +0,0 @@
-export default function PageTitle({ children, className }) {
-  return (
-    
-    <h2 className={`uppercase text-scblue text-5xl text-center font-extrabold mt-28 mb-12 ${className}`}>
-      {children}
-    </h2>
-  );
-}
