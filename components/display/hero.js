20bdc18d1c8f2a76f0817ab9d473cc6729f73aa1
mohansky <38560439+mohansky@users.noreply.github.com>
2023-04-03 09:52:15 +0530

    Add files via upload

--- components/display/hero.js
+++ components/display/hero.js
@@ -1,7 +1,6 @@
-import ButtonOutline from "@ui/button-outline";
 import Button from "@ui/button";
 import Image from "next/image";
-import Link from "next/link"; 
+import Link from "next/link";
 import { imgblurDataURL } from "../../lib/constants";
 
 export default function Hero({ item }) {
@@ -12,7 +11,7 @@ export default function Hero({ item }) {
         <div className="w-full h-full absolute top-0 left-0 z-0 bg-top">
           <Image
             src={item.img}
-            alt={item.subtitle} 
+            alt={item.subtitle}
             width={1920}
             height={1080}
             placeholder="blur"
@@ -25,19 +24,13 @@ export default function Hero({ item }) {
         <div className="lg:w-3/4 xl:w-2/4 relative z-50 h-100 lg:mt-16">
           <div>
             <h1 className="text-white text-4xl md:text-5xl xl:text-6xl font-bold leading-tight">
-            {item.subtitle} 
+              {item.subtitle}
             </h1>
             <h2 className="text-slate-100 text-xl md:text-3xl leading-snug mt-4 ">
               {item.title}
             </h2>
             <Link href={item.btnUrl}>
-              {/* <ButtonOutline>
-              {item.btnText}
-              </ButtonOutline> */}
-              <Button>
-              {item.btnText}
-              </Button>
-
+              <Button>{item.btnText}</Button>
             </Link>
           </div>
         </div>
--- components/display/page-title.js
+++ components/display/page-title.js
@@ -1,7 +1,7 @@
 export default function PageTitle({ children, className }) {
   return (
     
-    <h2 className={`uppercase text-scblue text-5xl text-center font-extrabold mt-10 mb-12 ${className}`}>
+    <h2 className={`uppercase text-scblue text-5xl text-center font-extrabold my-12 ${className}`}>
       {children}
     </h2>
   );
