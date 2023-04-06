bd5983b0814d5fc50f0de9d767c45a5c4c48cbf9
mohansky <38560439+mohansky@users.noreply.github.com>
2023-04-06 11:11:35 +0530

    Add files via upload

--- components/display/carousel.js
+++ components/display/carousel.js
@@ -3,17 +3,17 @@ import { Icon } from "@iconify/react";
 
 function NextArrow({onClick}) {
   return (
-    <button className="absolute hidden md:block top-1/2 right-10 text-scblue z-40 animate-pulse focus:animate-ping" onClick={onClick} aria-label="next">
+    <button className="absolute p-2 bg-white rounded-full hidden md:block top-1/2 right-10 text-scblue z-40 animate-pulse active:scale-105" onClick={onClick} aria-label="next">
       <Icon icon="bi:arrow-right" className="w-8 h-8" />
     </button>
   );
 }
 
 function PrevArrow({onClick}) {
-  return (
-    <button className="absolute hidden md:block top-1/2 left-10 text-scblue z-40 animate-pulse focus:animate-ping" onClick={onClick} aria-label="prev">
+  return ( 
+    <button className="absolute p-2 bg-white rounded-full hidden md:block top-1/2 left-10 text-scblue z-40 animate-pulse active:scale-105" onClick={onClick} aria-label="prev">
       <Icon icon="bi:arrow-left" className="w-8 h-8" />
-    </button>
+    </button> 
   );
 }
 
