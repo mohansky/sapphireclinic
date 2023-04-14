c77a65a33edf0a24a785354dc6d778acb660a39b
mohansky <38560439+mohansky@users.noreply.github.com>
2023-04-14 11:06:10 +0530

    Add files via upload

--- components/display/testimonials.js
+++ components/display/testimonials.js
@@ -1,14 +1,18 @@
 import TestimonialItem from "@display/testimonial-item";
 import TestimonialsList from "@data/testimonials.json";
 import SliderCarousel from "@display/carousel";
+import Image from "next/image";
 
 export default function Testimonials() {
   return (
     <>
-      <div
-        className="bg-no-repeat bg-cover bg-center relative h-full"
-        style={{ backgroundImage: "url(" + "/images/testimonial.jpg" + ")" }}
-      >
+      <div className="bg-no-repeat bg-cover bg-center relative h-full">
+        <Image
+          className="object-cover"
+          src="/images/testimonial.jpg"
+          alt=""
+          fill
+        />
         <div className="w-full h-full absolute top-0 left-0 bg-black/30 backdrop-blur-sm"></div>
         <div className="py-20">
           <SliderCarousel>
